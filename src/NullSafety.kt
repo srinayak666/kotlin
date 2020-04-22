5b0d7597a1555f0c23f7d778225adbf6ec3fe31a
sri <srinayak666@gmail.com>
2020-04-22 14:35:14 +0530

    Null Safety in Kotlin

---
+++ src/NullSafety.kt
@@ -0,0 +1,17 @@
+fun main(args:Array<String>){
+
+    var name:String="newString"
+   // name=null //this line will cause an issue : Error:(4, 10) Kotlin: Null can not be a value of a non-null type String
+    //to make name redeclare as nul we need to specify String with ? signifying type safety
+    println(name)
+
+    var text:String?="some text"
+    text=null
+    println(text) //OutPut: null
+
+    var data:String?
+    data=null
+    //println(data!!) //Exception in thread "main" kotlin.KotlinNullPointerException
+    data="data"
+    println(data!!) //!! this variable data should have to have a value;
+}
