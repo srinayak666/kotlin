cbefc8d35f5f16df6476bbdfab6bef8438e834db
sri <srinayak666@gmail.com>
2020-04-23 10:57:58 +0530

    Math Operation by reading Data from console

---
+++ src/operationsAndpriorities/MathOperations.kt
@@ -0,0 +1,11 @@
+package operationsAndpriorities
+
+fun main(args:Array<String>){
+
+    /*A simple calculation reading data from console*/
+    var n1:Float= readLine()!!.toFloat()
+    var n2:Float=readLine()!!.toFloat()
+    var sum:Float?
+    sum=n1+n2
+    println("Sum:"+sum)
+}
