b29922a9283e7ebe5e816b365e155360960c4ee8
sri <srinayak666@gmail.com>
2020-04-25 17:11:18 +0530

    do while loop

---
+++ src/loops/DoWhileLoop.kt
@@ -0,0 +1,13 @@
+package loops
+
+fun main(args:Array<String>){
+
+    //first run the code then check the condition..
+    //if condition is not valid then at least it will run once
+var initialValue=0
+    do {
+        println("Value: $initialValue")
+        initialValue++
+    }while(initialValue<=5)
+
+}
