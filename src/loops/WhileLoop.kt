362b7718bf4de9dc0a00db5776e01c37f9a7c714
sri <srinayak666@gmail.com>
2020-04-25 17:07:05 +0530

    while loop example

---
+++ src/loops/WhileLoop.kt
@@ -0,0 +1,11 @@
+package loops
+
+fun main(args:Array<String>){
+    var initialCondition=1
+    while(initialCondition<=5){
+        println("Printing inside while loop..$initialCondition")
+        initialCondition++
+    }
+
+
+}
