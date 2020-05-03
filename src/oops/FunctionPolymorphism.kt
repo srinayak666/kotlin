b1b5c7e84e70eb76c330c26648896cd129692447
sri <srinayak666@gmail.com>
2020-05-03 13:15:55 +0530

    Function plymorphism

---
+++ src/oops/FunctionPolymorphism.kt
@@ -0,0 +1,20 @@
+package oops
+
+fun main(args:Array<String>){
+    display("Hello")
+    display(20)
+    display(22.77)
+
+}
+/*
+Having a function with same name, same number of parameters with different Data Types is called Polymorphism
+ */
+fun display(data:String):Unit{
+ println("data: $data")
+}
+fun display(data:Int):Unit{
+    println("data: $data")
+}
+fun display(data:Double):Unit{
+    println("data: $data")
+}
