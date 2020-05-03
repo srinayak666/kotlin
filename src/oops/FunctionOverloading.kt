5d77125ecd077b7fe5cdd64618dc95050bf6e51d
sri <srinayak666@gmail.com>
2020-05-03 13:09:14 +0530

    Function Overloading

---
+++ src/oops/FunctionOverloading.kt
@@ -0,0 +1,16 @@
+package oops
+
+fun main(args:Array<String>){
+
+    println("Sum with Int's:${sum(10,20)}")
+    println("Sum with Floats's:${sum(10.4,20.3)}")
+    println("Sum with 3 Int's:${sum(10,20,50)}")
+}
+
+
+fun sum(n1:Int,n2:Int):Int{
+    return n1+n2
+}
+fun sum(n1:Int,n2:Int,n3:Int):Int{ //overloading sum function which holds 3 parameters
+    return n1+n2+n3
+}
